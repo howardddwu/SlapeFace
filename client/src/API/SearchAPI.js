2d7b97aef4d20c3909fdb2c3db5bcd490c339a47
howardddwu@gmail.com <howardddwu@gmail.com>
2023-04-27 22:03:16 -0400

    mini change

--- client/src/API/SearchAPI.js
+++ client/src/API/SearchAPI.js
@@ -9,7 +9,17 @@ export const SearchProphecy = async (dataFrom, setProphecies) => {
         await API.post('/search/searchProphecy', dataFrom)
             .then(
                 (res) => {
-                    const { data } = res;
+                    let { data } = res;
+                    data = data.sort((objA, objB) => {
+                        if (objA.numUser > objB.numUser) return -1
+                        if (objB.numUser > objA.numUser) return 1
+                        //if prophecies having same number of user participated, then display it by time
+                        return (
+                            (Number(new Date(objA.createAt)) -
+                                Number(new Date(objB.createAt))) *
+                            -1
+                        )
+                    })
                     setProphecies(data)
                 }
             )
